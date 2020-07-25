51ecde309a7ed75fc507a8577b6dc6245537984a
Breno Carvalho <Breno Carvalho>
2020-07-24 21:09:48 -0300

    Window update

--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
 import pygame
 
-pygame.init()
+pygame.init() #Starting PyGame
 
+#Windows settings
 WIDTH, HEIGHT = 640, 480
+background_color = (255,255,255)
 
-window = pygame.display.set_mode([WIDTH, HEIGHT])
-pygame.display.set_caption("Snake Game")
+window = pygame.display.set_mode([WIDTH, HEIGHT]) #Set window size
+pygame.display.set_caption("Snake Game") #Set window title
 
 while True:
-    for event in pygame.event.get():
+    for event in pygame.event.get(): #Get PyGame events
         if event.type == pygame.QUIT:
             quit()
 
-    pygame.display.update()
+    window.fill(background_color) #Painting background color
+
+    pygame.display.update() #Updating screen
